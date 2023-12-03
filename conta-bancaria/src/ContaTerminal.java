c7881f5122a5d75aa2ff9f48925a65fda246e24e
Renan <renanmirandae@gmail.com>
2023-12-03 13:51:28 -0300

    Criando a classe ContaTerminal

---
+++ conta-bancaria/src/ContaTerminal.java
@@ -0,0 +1,93 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * @author Renan Miranda
+ * @since 03/12/2023
+ * @version 1.0
+ * */
+
+public class ContaTerminal {
+    /**
+     * @param numeroConta Armazena o numero da conta do usuario
+     * @param agencia Armazena o numero da agencia
+     * @param nomeCliente Armazena o nome do cliente
+     * @param saldo Armazena o saldo da conta de cada usuario
+     * */
+    int numeroConta;
+    String agencia, nomeCliente;
+    double saldo;
+
+    public ContaTerminal()
+    {
+    }
+
+    /**
+     * @return Cria uma conta para um novo usuario e retorna uma String informando se a criação for bem sucedida
+     * */
+    public void criarConta()
+    {
+        //String agencia, int numeroConta, String nomeCliente, double saldo
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da agencia: ");
+        String numeroAgencia = scanner.next();
+        setAgencia(numeroAgencia);
+
+        System.out.println("Digite o numero da conta:");
+        int numeroConta = scanner.nextInt();
+        setNumeroConta(numeroConta);
+
+        System.out.println("Digite seu nome (Apenas o primeiro nome): ");
+        String nomeCliente = scanner.next();
+        setNomeCliente(nomeCliente);
+
+        System.out.println("Digite o valor do deposito de abertura da conta: (Apenas numeros)");
+        double saldo = scanner.nextDouble();
+        setSaldo(saldo);
+    }
+
+    public String mensagemContaCriadaSucesso()
+    {
+        return "Olá " + getNomeCliente() + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                getAgencia() + " conta " + getNumeroConta() +  " e seu saldo de R$" +
+                getSaldo() + " já está disponível para saque.";
+    }
+    public String mensagemFalhaCriarConta()
+    {
+        return "Erro ao criar a conta, verifique os dados digitados e tente novamente";
+    }
+
+
+    private void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    private void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    private void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    private void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
--- conta-bancaria/src/Main.java
+++ conta-bancaria/src/Main.java
@@ -1,17 +1,19 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.InputMismatchException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
+        ContaTerminal novaContaBancaria = new ContaTerminal();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Seja Bem-vindo. Por favor digite seus dados para criar uma conta em nosso banco");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            novaContaBancaria.criarConta();
+            System.out.println(novaContaBancaria.mensagemContaCriadaSucesso());
+
+        }catch(Exception e) {
+            System.out.println(novaContaBancaria.mensagemFalhaCriarConta());
+            novaContaBancaria.criarConta();
+            System.out.println(novaContaBancaria.mensagemContaCriadaSucesso());
         }
     }
 }
